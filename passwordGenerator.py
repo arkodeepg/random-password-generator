37e84872b8bf363eca7b40f4cc27d2e5172887d8
Arkodeep Ghosh <arkodeep01ghosh@gmail.com>
2019-12-23 23:03:13 +0530

    Create passwordGenerator.py
    thsi file contains the basic backend stuff of the password generator

---
+++ passwordGenerator.py
@@ -0,0 +1,15 @@
+from random import *
+
+small = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+large = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+special = ['!', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+cho = [small, large, num, special]
+shuffle(cho)
+pswd = []
+for i in cho:
+    temp = sample(i, randint(1, 3))
+    for j in temp:
+        pswd.append(j)
+shuffle(pswd)
+print("".join(pswd))
