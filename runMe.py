626f3bff458317527b31d607642773820c75efd0
Arkodeep Ghosh <arkodeep01ghosh@gmail.com>
2019-12-24 22:10:56 +0530

    changed the icon

--- runMe.py
+++ runMe.py
@@ -4,11 +4,11 @@ import tkinter.messagebox
 
 root = Tk()
 root.title("random password")
-root.geometry("275x50")
+root.iconbitmap(r'key.ico')
+root.geometry("275x50+500+500")
 var = StringVar()
 label = Label(root, textvariable = var, font="Helvetica 12")
 var.set(password())
 label.pack(padx = 0.5, pady = 5, anchor = CENTER)
 
 root.mainloop()
- 
